af338dfb74950df2a028861ac69235cc8a6ab9ed
leemms102 <leemms102@gmail.com>
2023-01-26 17:16:18 +0900

    순수 자바 코드로 가짜 객체 넣어줌

---
+++ src/test/java/com/example/springcore/order/OrderServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.example.springcore.order;
+
+import com.example.springcore.discount.FixedDiscountPolicy;
+import com.example.springcore.member.Grade;
+import com.example.springcore.member.Member;
+import com.example.springcore.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixedDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
