4cfb82fc801ef89f20b323b50c9e436df6999f24
leemms102 <leemms102@gmail.com>
2023-01-26 17:11:29 +0900

    의존관계 명시 테스트

--- src/main/java/com/example/springcore/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/springcore/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package com.example.springcore.discount;
 
 import com.example.springcore.member.Grade;
 import com.example.springcore.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("mainDiscountPolicy")    // 2. @Qualifier 이름으로 의존관계 명시
+@Primary    // 3. @Primary는 가정 우선적으로 의존관계 주입
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
     @Override
