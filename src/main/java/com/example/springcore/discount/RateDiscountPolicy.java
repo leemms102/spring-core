135bd841b3069501df871a9ae5c52b4dffc5ec05
leemms102 <leemms102@gmail.com>
2023-01-27 17:14:20 +0900

    @primary @qualifier custom annotation

--- src/main/java/com/example/springcore/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/springcore/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package com.example.springcore.discount;
 
+import com.example.springcore.annotation.MainDiscountPolicy;
 import com.example.springcore.member.Grade;
 import com.example.springcore.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("mainDiscountPolicy")    // 2. @Qualifier 이름으로 의존관계 명시
+// @MainDiscountPolicy 4. custom annotation
 @Primary    // 3. @Primary는 가정 우선적으로 의존관계 주입
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
