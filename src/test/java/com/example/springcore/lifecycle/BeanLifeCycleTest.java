6dfb501fb35b5cbec127d94c4724b959d5a56772
leemms102 <leemms102@gmail.com>
2023-01-27 17:19:56 +0900

    생명주기 callback

---
+++ src/test/java/com/example/springcore/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,27 @@
+package com.example.springcore.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    public void lifeCycleTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient clinet = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+
+            return networkClient;
+        }
+    }
+}
