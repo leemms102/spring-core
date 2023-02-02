d06c444a2e7dd405bd37c38338e677f5294a23c8
leemms102 <leemms102@gmail.com>
2023-02-02 11:50:36 +0900

    생명주기 콜백

--- src/test/java/com/example/springcore/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/example/springcore/lifecycle/BeanLifeCycleTest.java
@@ -9,13 +9,17 @@ public class BeanLifeCycleTest {
     @Test
     public void lifeCycleTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
-        NetworkClient clinet = ac.getBean(NetworkClient.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
         ac.close();
     }
 
     @Configuration
     static class LifeCycleConfig {
 
+//        스프링 빈 생명주기 콜백 - 2. 빈 등록 초기화, 소멸 메소드
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        // 설정 정보이기 때문에 외부 라이브러리에도 적용가능
+        // initMethod, destroyMethod 명시 안해도 메소드 이름으로 추론해서 호출
         @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
--- src/test/java/com/example/springcore/lifecycle/NetworkClient.java
+++ src/test/java/com/example/springcore/lifecycle/NetworkClient.java
@@ -1,13 +1,21 @@
 package com.example.springcore.lifecycle;
 
-public class NetworkClient {
+// 스프링 빈 생명주기 콜백 - 1. 인터페이스
+// @Override afterPropertiesSet(), destroy(): 매우 초창기 방법
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {    // implements InitializingBean, DisposableBean
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+//        connect();
+//        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -20,11 +28,30 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call: " + url + "message = " + message);
+        System.out.println("call: " + url + " message = " + message);
     }
 
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
     }
+
+
+    // 스프링 빈 생명주기 콜백 - 3. 어노테이션 @PostConstruct, @PreDestroy
+    // 가장 권장, 자바 표준 기술
+    // 외부 라이브러리에서 사용 못함
+
+    // 의존관계 주입이 끝나면 초기화 작업 실행
+    @PostConstruct
+    public void init() {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 소멸되기 전 실행
+    @PreDestroy
+    public void close() {
+        disconnect();
+    }
+
 }
